cd7abbbe71570d8de8ab5b75cab97b05b2e89e26
Daniel Hackner <dan.hackner@gmail.com>
2016-05-24 15:18:31 -0700

    Add DateStatus model

--- runmoar/running_calendar/models.py
+++ runmoar/running_calendar/models.py
@@ -2,4 +2,14 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class DateStatus(models.Model):
+
+    class Meta:
+        ordering = ('date', )
+
+    date = models.DateField(unique=True)
+    completed = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return '{0.date} {0.completed}'.format(self)
