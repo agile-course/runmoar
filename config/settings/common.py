160c499494beeff7576bec06b98f42d9686cab7a
Daniel Hackner <dan.hackner@gmail.com>
2016-05-29 11:35:51 -0700

    Add running_calendar app

--- config/settings/common.py
+++ config/settings/common.py
@@ -44,7 +44,7 @@ THIRD_PARTY_APPS = (
 # Apps specific for this project go here.
 LOCAL_APPS = (
     'runmoar.users',  # custom users app
-    # Your stuff: custom apps go here
+    'runmoar.running_calendar',
 )
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
